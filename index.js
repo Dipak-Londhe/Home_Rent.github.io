372b384c61ac8a2893a16185552095dd4bcbea18
Dipak-Londhe <dipaklondhe6511@gmail.com>
2022-12-18 15:34:36 +0530

    Home page

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const opensidebar = ()=>{
+    document.getElementsByClassName('Header')[0].style.transform = "translateX(0)";
+    
+}
+
+const closesidebar = ()=>{
+    document.getElementsByClassName('Header')[0].style.transform = "translateX(-500%)";
+    
+}
